77b0740fc21bfa15a11a32841fb316c9d03d74ef
John Detter <jdetter@wisc.edu>
2015-10-24 14:09:27 -0500

    Added stub files

--- performance/performance4.c
+++ performance/performance4.c
@@ -1,1 +1,62 @@
-int main(int argc, char** argv) {return -1;}
+/**
+ * Authors:
+ * 	John Detter <jdetter@wisc.edu>
+ * 	Riccardo Mutschlechner <riccardo@cs.wisc.edu> 
+ */
+
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
+
+#include "bench.h"
+	
+/* Page size on x86 is 4096 */
+#define PGSIZE 0x1000
+
+int main(int argc, char** argv)
+{
+	ull min = (ull)-1;
+
+	int x;
+	for(x = 0;x < 100000;x++)
+	{
+		/* Page size on x86 is 4096 */
+		ull start;
+		ull end;
+		ull diff;
+		RDTSCP(start);
+		ptr = malloc()
+		RDTSCP(end);
+
+		if(!ptr)
+		{
+			printf("MMAP failed!\n");
+			return -1;
+		}
+
+		diff = end - start;
+		if(diff < min) 
+			min = diff;
+	}
+
+	int file = open(OUTPUT_FILE, O_RDWR | O_CREAT | O_APPEND, 0664);
+	if(file < 0)
+	{
+		printf("Open failed!\n");
+		return -1;
+	}
+
+	char num[512];
+	snprintf(num, 512, "%d\n", min);
+	int written = write(file, num, strlen(num));
+	if(written != strlen(num))
+	{
+		printf("Write failed!\n");
+		return -1;
+	}
+
+	return 0;
+}
