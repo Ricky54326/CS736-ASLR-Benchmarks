58f44c4a895b7d318f9b023412dd721b1fdcf3c1
Ricky Mutschlechner <mutschlechne@wisc.edu>
2015-11-21 19:37:00 -0600

    updated graphing script and entropy experiment script to work properly

--- entropy1.r
+++ entropy1.r
@@ -1,7 +1,11 @@
+jpeg("entropy1.png");
 vec <- scan("http://www.upl.cs.wisc.edu/~riccardo/entropy.csv")
 
 barplot(vec, 
 		xlab="OS",
 		ylab="# Collisions", 
     	main="ASLR Stack Entropy",
-    	names.arg=c("Debian", "FreeBSD", "HBSD"))
+    	names.arg=c("Debian", "FreeBSD", "HBSD"),
+    	col=c("red", "orange", "blue"))
+
+dev.off()
